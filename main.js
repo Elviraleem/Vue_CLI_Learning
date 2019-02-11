f2306bec721eef96e4e2b3d34f5d73e00c0fcd93
Elvira Fernández Alonso <elviraleem@MacBook-Pro-de-Elvira.local>
2019-02-11 14:33:59 +0100

    Add template in the DOM with an "id"  and as a string in the component

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
-Vue.component('mis_tareas', { template:`<ul>
-    <li v-for="tarea in tareas">{{ tarea.title }}
-    </li>
-</ul>`,
-     mounted(){
-          axios.get('https://jsonplaceholder.typicode.com/todos')
-          .then((response) => {
-               this.tareas = response.data;
-     
-          });
-         },
-         
-         data (){
-              return{
-               tareas: [],
-              }
-            
-            
-         },
-         
-});
+Vue.config.devtools = true
+
+Vue.component ('elegir-ganador', {
+     props: ['listado'],
+     template: '#elegir-ganador-template',
+     methods:{
+          elegirGanador(){
+               cantidad = this.participantes.length;
+               indice = Math.floor((Math.random()) * cantidad);
+               this.ganador = this.participantes [indice-1];
+          }
+     },
+     data(){
+          return{
+               ganador: false,
+               participantes: this.listado
+          }
+     }
+
+})
 
 new Vue ({
     el: 'main',
+    data:{
+         personas:[
+              "Anna", "Ling", "Andras", "Claudia", "Ernest", "Elvira"
+          ]
+    }
     
         
 });
